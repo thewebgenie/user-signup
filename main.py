8ae164d1ee5a6c071314718281d2a0f8e0570bb0
Ryan Zajac <ryan4life12@gmail.com>
2017-10-02 14:14:55 -0500

    added css styling, including rules that gives input fields with errors a red border

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/')
-def index():
-    return render_template('user_signup_form.html', title="Signup Form")
-
-def isblank(field):
+#DRY helper functions
+def is_blank(field):
     if field == '':
         return True
     else:
         return False
+def len_test(field, min, max):
+    if len(field) < min or len(field) > max:
+        return True
+    else:
+        return False
+def contains_space(field):
+    if " " in field:
+        return True
+    else:
+        return False
     
+@app.route('/')
+def index():
+    return render_template('user_signup_form.html', title="Signup Form")  
+
 @app.route('/register', methods=['POST'])
 def validate():
     #get form values and store as local variables
@@ -29,29 +40,76 @@ def validate():
     verpassword_error = ''
     email_error = ''
     
-    if isblank(username):
-        username_error = 'Username field must not be blank'
-        #return render_template('user_signup_form.html',username_error=username_error, title="Error")
-    if isblank(password):
-        password_error = 'Password field must not be blank'
-        #return render_template('user_signup_form.html',password_error=password_error, title="Error")
-    if isblank(verpassword):
-        verpassword_error = 'Verify Password field must not be blank'
-    elif password != verpassword:
+    #create blank classes for inputs that have validation errors
+    uclass = ''
+    pclass = ''
+    vpclass = ''
+    eclass = ''
+    
+    #username validations
+    if is_blank(username):
+        username_error = 'Username must not be blank'
+    elif contains_space(username):
+        username_error = 'Username must not contain any spaces'
+    elif len_test(username, 3, 20):
+        username_error = 'Username must be 3 - 20 characters in length'
+        
+    if username_error != '':
+        uclass = 'fieldError'
+
+        
+    #Password validations
+    if is_blank(password):
+        password_error = 'Password must not be blank'
+    elif contains_space(password):
+        password_error = 'Password must not contain any spaces'
+    elif len_test(password, 3, 20):
+        password_error = 'Password must be 3 - 20 characters in length'
+        
+    if password_error != '':
+        pclass = 'fieldError'
+        
+    #Verify Password Validation
+    """if is_blank(verpassword):
+        verpassword_error = 'Verify Password field must not be blank'"""
+    if password != verpassword:
         verpassword_error = 'Verify Password must match Password'
         password = ''
         verpassword = ''
         
-    if username_error or password_error or verpassword_error or email_error:
+    if verpassword_error != '':
+        vpclass = 'fieldError'
+        
+    #Email Validation
+    if not is_blank(email):#if email field is not blank
+        #Do these validation statements
+        if len_test(email, 3, 20):
+            email_error = 'Email must be 3 - 20 characters in length'
+        elif " " in email:
+            email_error = 'Email must not contain any spaces'
+        elif "." not in email or "@" not in email:
+            email_error = 'Email must be in format "something"@"something".com'
+            
+    if email_error != '':
+        eclass = 'fieldError'
+        
+    if username_error or password_error or verpassword_error or email_error: #if errors
+        #render user_signup_form with error messages
         return render_template('user_signup_form.html',username=username,
                            password=password,
                            verpassword=verpassword,
                            email=email,
                            username_error=username_error, 
+                           uclass=uclass,
                            password_error=password_error,
+                           pclass=pclass,
                            verpassword_error=verpassword_error,
+                           vpclass=vpclass,
+                           email_error=email_error,
+                           eclass=eclass,
                            title='Error')
-    if not isblank(username)and not isblank(password) and not isblank(verpassword):
+    else:
+        #render welcome.html
         return render_template('welcome.html',username=username, title='Welcome')
 
 app.run()
